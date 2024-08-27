f2e10fea769bf7ae9914aecf717f69c2f6a06669
Shreya Singh <117816702+Shreya-sgh@users.noreply.github.com>
2024-08-27 17:45:55 +0530

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,39 @@
+<?php      
+    $host = "localhost";  
+    $user = "root";  
+    $password = '';  
+    $db_name = "javatpoint";  
+      
+    $con = mysqli_connect($host, $user, $password, $db_name);  
+    if(mysqli_connect_errno()) {  
+        die("Failed to connect with MySQL: ". mysqli_connect_error());  
+    }  
+?>  
+
+
+spotify:
+"https://open.spotify.com/embed/track/1Zi1HxNIWVFHx1RxOXkTjB?utm_source=generator"
+
+spotify:
+  " https://open.spotify.com/embed/track/5Se9PXWc57LRT0zthjI5cG?utm_source=generator" 
+
+spotify:
+          "https://open.spotify.com/embed/track/6VBhH7CyP56BXjp8VsDFPZ?utm_source=generator"
+
+ spotify:
+          " https://open.spotify.com/embed/track/7Els4IIFYa4P4RxBkZkxdd?utm_source=generator"  
+
+          spotify:
+          " https://open.spotify.com/embed/track/1ax8ZuwRVkSdzzsIqyCNWQ?utm_source=generator"  
+
+
+          spotify:
+          "https://open.spotify.com/embed/track/208sMwgVcaFt2mT79Df1KG?utm_source=generator"
+
+          spotify:
+          "https://open.spotify.com/embed/track/17LZzRCY0iFWlDDuAG7BlM?utm_source=generator"  
+
+          spotify:
+       "https://open.spotify.com/embed/track/6FAYpZ4jve8vpvTwUvjK6H?utm_source=generator"    
+
+
---
+++ music_list.js
@@ -0,0 +1,67 @@
+let trackList= [
+  {
+      name: "Sankat Mochan",
+      path: "music1.aac",
+      img: "https://i.pinimg.com/originals/de/88/f0/de88f02411a5e824181f6d2aacd1a1c0.jpg",
+      singer:"Agam Agarwal",
+      
+ },
+ {
+      name: "Woh Krishna hai",
+      path: " Music2.mp3" ,
+      img: " https://i.pinimg.com/736x/a3/16/99/a3169950c687a5a53c7dbfec99f31f99.jpg ",
+      singer:" Sukhwinder Singh",
+      
+  },
+
+  {
+    name: "Kesariya",
+    path: "  Music3.mp3 ",
+      img:" https://static.toiimg.com/photo/msid-92802278/92802278.jpg",
+      singer:" Arijit Singh",
+      
+  },
+  {
+    name: "Apna bana le",
+    path: " Music4.mp3  ",
+      img: " https://stat4.bollywoodhungama.in/wp-content/uploads/2022/11/Apna-Bana-Le-Bhediya-Varun-Dhawan-Kriti-Sanon.jpg ",
+      singer:" Arijit Singh",
+     
+  },
+  {
+    name: "Tumse Milke",
+    path: "  Music5.mp3 ",
+      img:" https://images.news18.com/ibnlive/uploads/2021/11/shah-rukh-khan-1.jpg ",
+      singer:"Sonu Nigam",
+     
+  },
+ 
+{
+    name: "Sweetheart",
+    path: "  Music6.mp3 ",
+      img: "https://images.indianexpress.com/2018/11/kedarnath-song-759.jpg ",
+      singer:" Amit Trivedi",
+      
+},
+  {
+    name: " Nacho Nacho ",
+    path: " Music7.mp3 ",
+      img:"  https://www.goldderby.com/wp-content/uploads/2022/12/rrr-naatu-naatu.jpg?w=620",
+      singer:" Rahul Sipligunj",
+     
+  },
+  {
+    name: " Patola",
+    path: " Music8.mp3  ",
+      img:" https://images.firstpost.com/wp-content/uploads/2018/03/patola-1280.jpg",
+      singer:"Guru Randhawa",
+      
+  },
+  {
+      name: "Jhoome jo pathaan",
+      path: " Music9.mp3  ",
+      img: " https://images.mid-day.com/images/images/2022/dec/jhoome-jo-pathaan_d.jpg",
+      singer:"Arijit Singh",
+  },
+
+];
